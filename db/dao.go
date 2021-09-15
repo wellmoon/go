8214a60c6ad4f38fbdb564e80903bb2905021484
zhangwenjie <13810010059@126.com>
2021-09-15 10:27:40 +0800

    update

--- db/dao.go
+++ db/dao.go
@@ -39,7 +39,7 @@ func NewDao(ip string, port string, dbUser string, dbPass string, dbName string)
 func (dao Dao) QueryMap(sql string, args ...interface{}) (map[string]string, error) {
 	rows, err := dao.db.Query(sql, args...)
 	if err != nil {
-		Log.Error("查询失败，sql is {}, err : {}", sql, err)
+		Log.Error("queryMap error, sql is {}, err : {}", sql, err)
 		return nil, err
 	}
 	defer rows.Close()
@@ -69,9 +69,9 @@ func (dao Dao) QueryMap(sql string, args ...interface{}) (map[string]string, err
 func (dao Dao) QueryList(sql string, args ...interface{}) (*ListResult, error) {
 	t1 := time.Now()
 	rows, err := dao.db.Query(sql, args...)
-	Log.Debug("查询list耗时 %.2f 秒，sql is {}", time.Since(t1).Seconds(), sql)
+	Log.Debug("QueryList cost %.2f seconds, sql is {}", time.Since(t1).Seconds(), sql)
 	if err != nil {
-		Log.Error("查询失败，sql is {}, err : {}", sql, err)
+		Log.Error("QueryList error, sql is {}, err : {}", sql, err)
 		return nil, err
 	}
 	defer rows.Close()
